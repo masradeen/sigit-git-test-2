d617f57b0a23806ce07a0cad877ec1c8c568af1e
MasRadeen <sigitnugrohop@gmail.com>
2024-01-23 11:57:27 +0800

    menambahkan paragraf lagi

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus a eveniet tempore facere repellat minima, blanditiis eligendi explicabo omnis nulla ipsum quae corrupti repellendus, tempora sed, dicta veniam dolor eius?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est facere quae saepe, voluptatibus consectetur repellat maiores, sint veniam quam quisquam porro voluptatem ad corporis at rerum quidem cumque reprehenderit dicta!</p>
 </body>
 </html>
